bdae785a031628c98705a72d92bb90fde163bfc6
svburke <sean.burke2@nih.gov>
2023-01-09 12:39:08 -0600

    Update dbGaP_ConcatenatoR.R

--- dbGaP_ConcatenatoR.R
+++ dbGaP_ConcatenatoR.R
@@ -26,7 +26,7 @@ list_of_packages=c("readr","stringi","optparse","tools")
 
 #Based on the packages that are present, install ones that are required.
 new.packages <- list_of_packages[!(list_of_packages %in% installed.packages()[,"Package"])]
-suppressMessages(if(length(new.packages)) install.packages(new.packages))
+suppressMessages(if(length(new.packages)) install.packages(new.packages, repos = "http://cran.us.r-project.org"))
 
 #Load libraries.
 suppressMessages(library(readr,verbose = F))
